dbeadcd577f97731d29f35eca1fead02c5114c6e
adrew <adrew543@gmail.com>
2022-08-06 17:33:12 +1000

    add archive posts and began JS to show and hide content

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,24 @@
 console.log("Script loaded!");
 
+// find the anchor with the href that matches the current url and add a styling class to it 
 currentLinks = document.querySelectorAll('a');
-console.log(currentLinks);
 currentLinks.forEach(function(link) {
-  console.log('/'+link.getAttribute("href"))
-  console.log(window.location.pathname)
-
   if ('/' + link.getAttribute("href") == window.location.pathname && link.className == 'link') {
     link.className += ' currentPage';
   }
 }); 
+
+// event handler for clicking to expand an archive 
+// BUG: seems to periodically have the content return NULL, meaning the query select isn't grabbing it
+function showContent (event) {
+  console.log("CLICK")
+  content = event.target.parentNode.querySelector('#hide');
+  if (content.classList.contains('hidden')) {
+    content.classList.remove('hidden')
+  } else {
+    content.classList.add('hidden')
+  }
+}
+document.querySelectorAll("#expand").forEach( item => {
+  item.addEventListener("click", showContent);
+})
