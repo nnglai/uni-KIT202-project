41e4d58dbd042b861bc7f905e4ec60c6496098ba
Alex Drew <adrew543@gmail.com>
2022-08-09 17:21:06 +1000

    tweaks to archive JS

--- static/js/script.js
+++ static/js/script.js
@@ -12,17 +12,25 @@ currentLinks.forEach(function(link) {
 // event handler for clicking to expand an archive 
 // BUG: seems to periodically have the content return NULL, meaning the query select isn't grabbing it
 function showContent (event) {
-  content = event.target.parentNode.querySelector('#hide');
-  arrow = event.target.querySelector('i');
+  if(event.target.classList.contains("expand")){
+    content = event.target.parentNode.querySelector('.post-content');
+    arrow = event.target.querySelector('i');
 
-  content.classList.toggle("hidden");
+    content.classList.toggle("hidden");
 
-  arrow.classList.toggle("fa-angle-down");
-  arrow.classList.toggle("fa-angle-up");
+    arrow.classList.toggle("fa-angle-down");
+    arrow.classList.toggle("fa-angle-up");
+  }
+
+  //console.log(event.target.parentNode.querySelector('.post-content'));
+
+  //console.log(event.target);
 
 }
 if (window.location.pathname == '/archive.html') {
-  document.querySelectorAll("#expand").forEach( item => {
+  console.log(document.querySelectorAll(".expand"));
+  document.querySelectorAll(".expand").forEach( item => { //return to classic for loop rather than foreach =>
+    console.log(item);
     item.addEventListener("click", showContent);
   })
 }
